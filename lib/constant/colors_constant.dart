5d7f0a3c1c2e2a2887c35eb16c65e61ebf0df887
axcamz <61030878+axcamz@users.noreply.github.com>
2021-03-11 22:54:44 +0700

    initialize Project with HomeScreen

---
+++ lib/constant/colors_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const mRed = Color(0xFFEF4F4F);
+const mBlue = Color(0xFF74C7B8);
+const mCrime = Color(0xFFFFCDA3);
+const mBlueTitle = Color(0xFF158392);
